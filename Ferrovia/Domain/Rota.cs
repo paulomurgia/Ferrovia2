c55ee6ec09a886d6640ada93195421f1dfafdd8b
paulomurgia <62908663+paulomurgia@users.noreply.github.com>
2021-04-21 21:35:42 -0300

    Add files via upload

---
+++ Ferrovia/Domain/Rota.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ferrovia.Domain
+{
+    public class Rota
+    {
+        private Dictionary<string, int> _grafo;
+
+        public Rota()
+        {
+            _grafo = new Dictionary<string, int>();
+        }
+
+        public void AddRota(string nome, int distancia)
+        {
+            _grafo.Add(nome, distancia);
+        }
+
+        public int ObterDistancia(string rota)
+        {
+            var trecho = "";
+            var distanciaRota = 0;
+            var distanciaTrecho = 0;
+
+            trecho = rota.Replace("-", "");
+
+            if (_grafo.ContainsKey(trecho))
+                return _grafo[trecho];
+
+            var vtTrechos = rota.Split("-");
+
+            for (int i = 0; i < vtTrechos.Length - 1; i++)
+            {
+                distanciaTrecho = ObterDistanciaViagem(vtTrechos[i], vtTrechos[i + 1]);
+
+                if (distanciaTrecho == -1)
+                    return distanciaTrecho;
+
+                distanciaRota += distanciaTrecho;
+            }
+
+            return distanciaRota;
+        }
+
+        private int ObterDistanciaViagem(string origem, string destino)
+        {
+            int peso = -1;
+            string trecho = origem + destino;
+
+            if (_grafo.ContainsKey(trecho))
+                return _grafo[trecho];
+
+            return peso;
+        }
+    }
+}
---
+++ Ferrovia/Program.cs
@@ -0,0 +1,36 @@
+﻿using Ferrovia.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace Ferrovia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sRota = "";
+            var iDistancia = 0;
+            
+            Rota rota = new Rota();
+            rota.AddRota("AB", 5);
+            rota.AddRota("BC", 4);
+            rota.AddRota("CD", 8);
+            rota.AddRota("DC", 8);
+            rota.AddRota("DE", 6);
+            rota.AddRota("AD", 5);
+            rota.AddRota("CE", 2);
+            rota.AddRota("EB", 3);
+            rota.AddRota("AE", 7);            
+
+            Console.WriteLine("Informe a rota desejada: ");
+            sRota = Console.ReadLine().ToUpper();
+
+            iDistancia = rota.ObterDistancia(sRota);
+
+            if (iDistancia == -1)
+                Console.WriteLine("NAO EXISTE ROTA");
+            else
+                Console.WriteLine(iDistancia);
+        }        
+    }
+}
